819144e1e8e1487ebb55bde1e42ae547b93e858b
Gresdian <chris_da98@outlook.it>
2023-11-29 18:18:48 +0100

    exercise finished

--- js/script.js
+++ js/script.js
@@ -8,17 +8,21 @@ function createCell(num){
 
     return element;
 }
+const startButton = document.getElementById("play");
 
+startButton.addEventListener("click", function(){
+    
+    const grid = document.getElementById('grid');
+    
+    for(let i=0; i<100; i++){
+        let square = createCell(i+1);
+    
+        square.addEventListener('click', function(){
+            this.classList.toggle('clicked');
+            console.log(i+1);
+        })
+    
+        grid.appendChild(square);
+    }
 
-const grid = document.getElementById('grid');
-
-for(let i=0; i<100; i++){
-    let square = createCell(i+1);
-
-    square.addEventListener('click', function(){
-        this.classList.toggle('clicked');
-        console.log(i+1);
-    })
-
-    grid.appendChild(square);
-}
+})
