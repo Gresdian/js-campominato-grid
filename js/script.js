44356cd369b3c34560f141e2770ed9c1057cc8fb
Gresdian <chris_da98@outlook.it>
2023-11-29 17:10:25 +0100

    grid completed

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// FUNZIONE CHE CREA LA SINGOLA CASELLA DELLA GRIGLIA
+
+function createCell(num){
+    const element = document.createElement('div');
+    element.classList.add('square');
+
+    element.innerText = num;
+
+    return element;
+}
+
+
+const grid = document.getElementById('grid');
+
+for(let i=0; i<100; i++){
+    let square = createCell(i+1);
+
+    square.addEventListener('click', function(){
+        this.classList.toggle('clicked');
+        console.log(i+1);
+    })
+
+    grid.appendChild(square);
+}
