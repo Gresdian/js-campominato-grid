1964c135884ebc14d450cbc34dfb0994dc4bd6c3
Gresdian <chris_da98@outlook.it>
2023-11-29 18:22:37 +0100

    add comment js

--- js/script.js
+++ js/script.js
@@ -8,15 +8,18 @@ function createCell(num){
 
     return element;
 }
+//CREAZIONE BOTTONE INIZIO GIOCO
+
 const startButton = document.getElementById("play");
 
 startButton.addEventListener("click", function(){
     
     const grid = document.getElementById('grid');
-    
+   //CREAZIONE CICLO FOR PER PRESENTAZIONE TABELLA 10*10  
     for(let i=0; i<100; i++){
         let square = createCell(i+1);
-    
+ 
+       //IMPLEMENTAZIONE DELL'ELEMENTO SQAURE ALL'INTERNO DELLA GRIGLIA 
         square.addEventListener('click', function(){
             this.classList.toggle('clicked');
             console.log(i+1);
